32402d4b8051e7dbb3f1dd4ae957871762dcd9c4
Cha-er-si <>
2021-06-10 15:46:46 +0800

    Debugging

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Server Started on port', port));
+app.listen(port, () => console.log(`Server Started on ${port}`));
